fc388d86caf07f1df51c0976016174ca5d2c0fa4
Edward Tanguay (hm.win) <edward@tanguay.info>
2020-08-21 07:56:30 +0200

    work on intellisense

--- src/components/Flashcard.tsx
+++ src/components/Flashcard.tsx
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
+//import { qstr } from '../qtools/qstr';
+const qstr = require('../qtools/qstr');
 
 const cardBack = require('images/card_back.png');
-const qstr = require('../qtools/qstr');
 
 
 interface IFlashcard {
@@ -13,12 +14,16 @@ interface IFlashcard {
 
 class Flashcard extends Component<IFlashcard> {
 	render() {
+
+		const extra: string = qstr.capitalizeFirstLetter('test'); 
+
 		return (
 			<div className="flashcard">
 				<div className="cardArea">
 					<img src={cardBack} alt="greybox" />
 				</div>
 				<div className="infoArea">
+					<div className="extra">{extra}</div>
 					<div className="categoryKind"><span className="category">{this.props.category}</span> {qstr.capitalizeFirstLetter(this.props.kind)} </div>
 					<div className="front">{qstr.capitalizeFirstLetter(this.props.front)}</div>
 					<div className="back"><span>{this.props.back}</span></div>
