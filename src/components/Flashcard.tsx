a901a3ba4cda36ba15edbde7b37407ef34b0d6f5
Edward Tanguay <edward.tanguay@gefegmbh.local>
2020-08-21 15:02:07 +0200

    take out test in Flashcard.tsx

--- src/components/Flashcard.tsx
+++ src/components/Flashcard.tsx
@@ -13,15 +13,12 @@ interface IFlashcard {
 class Flashcard extends Component<IFlashcard> {
 	render() {
 
-		let extra: string = qstr.capitalizeFirstLetter('test');
-
 		return (
 			<div className="flashcard">
 				<div className="cardArea">
 					<img src={cardBack} alt="greybox" />
 				</div>
 				<div className="infoArea">
-					<div className="extra">{extra}</div>
 					<div className="categoryKind"><span className="category">{this.props.category}</span> {qstr.capitalizeFirstLetter(this.props.kind)} </div>
 					<div className="front">{qstr.capitalizeFirstLetter(this.props.front)}</div>
 					<div className="back"><span>{this.props.back}</span></div>
